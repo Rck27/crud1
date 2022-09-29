699874c65b8d84156f12bf4998feec6e1ec4504b
Rck27 <42706773+Rck27@users.noreply.github.com>
2022-09-29 22:38:52 +0700

    Update index.php
    add photo

--- index.php
+++ index.php
@@ -4,6 +4,25 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 	<title>Data Diri</title>
+	<style type="text/css">
+main {
+  padding: 0;
+}
+header {
+  position: sticky;
+  top:0;
+  bottom: 20;
+  padding:10px;
+  background: black;
+  color: white;
+  text-align: center;
+}
+
+content > div {
+  height: 100px;
+}
+
+	</style>
 </head>
 <body>
 	<script type="text/javascript">
@@ -12,19 +31,18 @@
 }
 	</script>
 
+<header>
 <h1 align="center">Basic Crud</h1>
-<hr>
 <form align="center" action="./index.php" method="POST">
 	<input type="submit" name="get" value="Get Data" class="btn btn-outline-primary">
 	<input type="submit" name="submit" value="Add/Edit" class="btn btn-outline-primary">
 </form>
-<hr>
-
-
+</header>
+<content>
 <?php
 include "config.php";
 if (isset($_POST["get"])) {
-	echo "<table border='1'><tr>
+	echo "<table border='1' align='center'><tr>
 		<th>No.</th>
 		<th>Nama</th>
 		<th>Email</th>
@@ -38,12 +56,18 @@ if (isset($_POST["get"])) {
 		echo "<th>".$row[1]."</th>";
 		echo "<th>".$row[2]."</th>";
 		echo "<th>".$row[3]."</th>";
-		echo "<th>".$row[4]."</th>";
+		$now = $row[4];
+		if($row[4] == NULL){
+			$now = "anon";
+		}
+
+		echo "<th><img src='./img/".$now.".png' width='100' height='100' onerror='this.onerror=null; this.src='anon.png''></th> ";
+		// echo "<th><img src='./img/".$row[4].' width='100' height='100'></th>";
 		echo "</tr>";
 	}
 }
 if( isset($_POST["submit"])){
-echo '<form method="post" action="./index.php">
+echo '<form method="post" action="./index.php" enctype="multipart/form-data">
 	<div class="form-group">
 		<label for="nama">Nama</label>
 		<input type="text" name="nama" class="form-control" placeholder="Nama Siswa" id="nama">
@@ -55,6 +79,9 @@ echo '<form method="post" action="./index.php">
 	<div class="form-group">
 		<label for="Alamat">Alamat</label>
 		<input type="text" name="alamat" class="form-control" placeholder="Alamat siswa" id="alamat">
+	</div>
+	<div>
+		<input type="file" name="image" accept="image/*" />
 	</div>
 <input type="submit" name="bt" class="btn btn-outline-primary">
 </form>';
@@ -74,14 +101,32 @@ if (isset($_POST["bt"])) {
 	$eml = $_POST["email"];
 	$almt = $_POST["alamat"];
 	#$sql = 'insert into murid(nama) values("erik")';
-	$sql = "INSERT INTO murid(Nama, email, alamat) VALUES('$nm', '$eml', '$almt')";
+	
+	$sql2 = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'kelas' AND TABLE_NAME = 'murid'";
+	$index = $conn->query($sql2)->fetch_row()[0];
+	echo $index;
+	#$index = 100;
+	$sql = "INSERT INTO murid(Nama, email, alamat, foto) VALUES('$nm', '$eml', '$almt', '$index')";
 	if (!$conn->query($sql)) {
-		echo "failed";
+		die("ewow");
 	}
+	echo $index;
+	$image_file = $_FILES['image'];
+	if (!isset($image_file)) {
+		die('no fiel uploaded');
+	}
+	if (!exif_imagetype($image_file["tmp_name"])) {
+		#header("Location: ./index.php")
+		die("not a picture");
+	}
+
+	echo $image_file["tmp_name"];
+	move_uploaded_file($image_file["tmp_name"],"./img/" . $index . ".png");
 }
 #echo $_GET["nama"];
 ?>
 </tr>
 </table>
+</content>
 </body>
 </html>
