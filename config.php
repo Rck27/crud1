36dfc2fa1678437a7f476d48e776372e1b1be913
Rck27 <42706773+Rck27@users.noreply.github.com>
2022-09-30 08:28:24 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $server = "localhost";
 $username = "root";
 $pwd = "";
 $dbname = "kelas";
-
+#CREATE TABLE `kelas`.`murid` (`No.` INT(10) NOT NULL AUTO_INCREMENT , `Nama` TEXT NOT NULL , `email` VARCHAR(100) NOT NULL , `alamat` TEXT NOT NULL , `foto` VARCHAR(50) NULL DEFAULT NULL , PRIMARY KEY (`No.`)) ENGINE = InnoDB;
 
 $conn = new mysqli($server, $username, $pwd, $dbname);
 
